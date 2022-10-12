deae4a3c4681017f550174db0f722d6353c3f3f6
Emre Bayır <emreebyrr@gmail.com>
2022-10-12 15:34:12 +0300

    FirstTouch Added,Bugs Fixed

--- Colorfull-Ball/Assets/Scripts/PlayerMovement.cs
+++ Colorfull-Ball/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     
    private Touch touch;
+
+   [Range(1,10)]
    public float speedModifier;
+
    private Rigidbody rb;
+
+   public int forwardSpeed;
+
+   public GameObject cam;
+   public Transform vectorback;
+   public Transform vectorforward;
+
+
+
+   
 
 public  void Start() 
 {
@@ -16,15 +29,34 @@ public  void Start()
 
    public void Update()
    {
+        if(Variables.firstTouch ==1)
+        {
+            transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+            cam.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+            vectorback.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+            vectorforward.transform.position += new Vector3(0,0,forwardSpeed*Time.deltaTime);
+        }
+
+
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Variables.firstTouch = 1;
+            }
 
             if(touch.phase == TouchPhase.Moved)
             {
                 rb.velocity = new Vector3         (touch.deltaPosition.x * speedModifier,
                                                   transform.position.y,
                                                   touch.deltaPosition.y * speedModifier);
+            }
+            else if(touch.phase ==TouchPhase.Ended)
+            {
+                //rb.velocity = new Vector3(0,0,0);
+                rb.velocity = Vector3.zero;
             }
         }
    }
