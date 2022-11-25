1c9a2b6826e87ac23589307b187671ebcc338ecd
Emre Bayır <72936805+emreebyr@users.noreply.github.com>
2022-11-25 14:24:00 +0300

    UI Buttons Added

--- Colorfull-Ball/Assets/Scripts/UIManager.cs
+++ Colorfull-Ball/Assets/Scripts/UIManager.cs
@@ -7,8 +7,70 @@ public class UIManager : MonoBehaviour
 {   
     public Image WhiteEffectImage;
     private int effectcontrol = 0;
-    public Button noads;
-    public Button shopcart;
+ 
+    public Animator layoutAnimator;
+
+    //buttons
+
+    public GameObject Settings_Open;
+    public GameObject Settings_Close;
+    public GameObject sound_on;
+    public GameObject sound_off;
+    public GameObject vibration_on;
+    public GameObject vibration_off;
+    public GameObject iap;
+    public GameObject information;
+
+
+    //buttons_functions
+
+    public void privacy_Policy()
+    {
+        Application.OpenURL("https://www.google.com.tr/");
+    }
+
+    public void TermOff_Use()
+    {
+        Application.OpenURL("https://www.google.com.tr/");
+    }
+
+    public void settings_Open()
+    {
+        Settings_Open.SetActive(false);
+        Settings_Close.SetActive(true);
+        layoutAnimator.SetTrigger("Slide_in");
+    }
+
+    public void settings_Close()
+    {
+        Settings_Open.SetActive(true);
+        Settings_Close.SetActive(false);
+        layoutAnimator.SetTrigger("Slide_out");
+    }
+
+    public void Sound_on()
+    {
+        sound_on.SetActive(false);
+        sound_off.SetActive(true);
+    }
+
+    public void Sound_off()
+    {
+        sound_on.SetActive(true);
+        sound_off.SetActive(false);
+    }
+
+    public void Vibration_on()
+    {
+        vibration_on.SetActive(false);
+        vibration_off.SetActive(true);
+    }
+
+    public void Vibration_off()
+    {
+        vibration_on.SetActive(true);
+        vibration_off.SetActive(false);
+    }
 
     public IEnumerator WhiteEffect()
     {
